ad020aa1ed1c2f7e36a468bf54897ddf90357f97
fabyelias <fabyeliasmarquez@gmail.com>
2026-07-31 22:42:33 -0300

    Apply Compose Compiler Gradle plugin to all Compose modules
    Desde Kotlin 2.0 el compilador de Compose se separo del plugin de Kotlin;
    todo modulo con buildFeatures { compose = true } necesita aplicar
    org.jetbrains.kotlin.plugin.compose explicitamente o el build falla con
    "Compose Compiler Gradle plugin is required when compose is enabled".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ import java.util.Properties
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
 }
