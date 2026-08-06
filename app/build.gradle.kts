e6eb4e56b91e44c4728b73132de14eb5cf01f8cd
fabyelias <fabyeliasmarquez@gmail.com>
2026-08-06 17:35:37 -0300

    CAMBIOS

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "0.1.0-skeleton"
+        versionName = "0.1.0"
     }
 
     signingConfigs {
